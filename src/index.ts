d1f40f2d3a31fa70d3ebf8093c0b209c323cb64b
Hyeonju Kim <hjk92go@gmail.com>
2023-04-13 23:50:17 +0900

    #5.4 JSDoc

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,28 @@
-const hello = () => "hi";
+// const hello = () => "hi";
 
-class Block {
-  constructor(private data: string) {}
-  static hello() {
-    return "hi";
-  }
-}
+// class Block {
+//   constructor(private data: string) {}
+//   static hello() {
+//     return "hi";
+//   }
+// }
 
-localStorage.getItem(); //ts는 브라우저 API의 타입을 어떠한 방식으로든 알고있다.
-//tsconfig.json lib에서 정의했기때문.
-//lib.dom.d.ts에 정의되어있다.
+// localStorage.getItem(); //ts는 브라우저 API의 타입을 어떠한 방식으로든 알고있다.
+// //tsconfig.json lib에서 정의했기때문.
+// //lib.dom.d.ts에 정의되어있다.
 
-import { init, exit } from "myPackage";
-//정의 파일(Declaration Files)이 찾을수 없다는 에러가 발생하는데,
-//정의 파일은 myPackage.d.ts에서 정의함(우리가 정의하는 함수를 의미)
+// import { init, exit } from "myPackage";
+// //정의 파일(Declaration Files)이 찾을수 없다는 에러가 발생하는데,
+// //정의 파일은 myPackage.d.ts에서 정의함(우리가 정의하는 함수를 의미)
 
-init({
-  url: "true",
-});
+// init({
+//   url: "true",
+// });
 
-exit(1);
+// exit(1);
 
-localStorage.clear();
+// localStorage.clear();
+
+//#5.4
+import { init, exit } from "./myPackage"; //TS파일에 마이패키지 파일을 불러온다는뜻
+//tsconfig.json에서 "allowJs": true 설정해줘야함.
--- src/myPackage.js
+++ src/myPackage.js
@@ -1,6 +1,33 @@
+// @ts-check
+//ts파일한테 js파일을 확인하라고 알리는것
+//ts의 보호는 받고싶은데 고장날까봐 걱정중일때 사용함!
+
+/**
+ * // @ts-check를 적어주고
+ * "allowJs": true 설정,
+ * 코멘드만 열심히 달아주면 사용법 끝
+ */
+
+/**
+ * Initializes the project(프로젝트를 초기화한다)
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns boolean
+ *
+ * ts가 이러한 코멘드를 읽고 체크해줌
+ * 당장 코드에 에러날 걱정을 하지 않아도 된다.
+ */
+
 export function init(config) {
   return true; //init은 config 오브젝트를 받고 true를 리턴함
 }
+
+/**
+ * Exits theh program(프로그램을 종료한다)
+ * @param {number} code
+ * @returns number
+ */
 export function exit(code) {
   return code + 1; //exit은 code 오브젝트를 받고 code+1을 리턴함.
 }
