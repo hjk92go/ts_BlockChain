d13bbcfb69d04a12efec568c6e8f6407ed042d5e
Hyeonju Kim <hjk92go@gmail.com>
2023-04-24 00:46:31 +0900

    #5.7 Chain

--- src/index.ts
+++ src/index.ts
@@ -22,3 +22,48 @@ class Block implements BlockShape {
     return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
+
+class Blockchain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+  private getPrevHash() {
+    //이전해쉬값을 불러오기
+    if (this.blocks.length === 0) return ""; //길이값이 0이라면 첫번째 해쉬값이 존재하지않기떄문에 ""리턴
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  public addBlock(data: string) {
+    //새로운 블록을 추가할 때, 블록에 저장하고싶은 데이터를 보내줘야함 / 데이터 타입 string
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+  public getBlocks() {
+    return [...this.blocks];
+  }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+blockchain.addBlock("Fourth one");
+
+console.log(blockchain.getBlocks());
+
+{
+  /**
+DefinitelyTyped
+TypeScript type 정의를 위한 리포지토리입니다.
+https://github.com/DefinitelyTyped/DefinitelyTyped
+
+@types/node
+npm i @types/node -D
+
+*/
+}
